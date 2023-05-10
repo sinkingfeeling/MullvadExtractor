8f284164f1546a06e342b35e789988ede73f9c0a
skarz <40373461+sinkingfeeling@users.noreply.github.com>
2023-05-10 06:40:09 -0700

    Update extract.js
    trying to simply extract the IP from each div one at a time

--- extract.js
+++ extract.js
@@ -2,34 +2,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function scrollToBottom() {
-    const checkInterval = 500;
-    let previousHeight = document.documentElement.scrollHeight;
-
-    while (true) {
-        window.scrollTo(0, document.documentElement.scrollHeight);
-        await sleep(checkInterval);
-
-        const currentHeight = document.documentElement.scrollHeight;
-        if (previousHeight === currentHeight) {
-            break;
-        }
-        previousHeight = currentHeight;
-    }
-}
-
-(async function() {
+async function main() {
     try {
         // Wait for 3 seconds before starting the script execution
         await sleep(3000);
 
-        // Scroll to the bottom of the page to load all elements
-        await scrollToBottom();
+        const container = document.querySelector('div[style*="overflow-y: auto"]');
+        if (!container) {
+            throw new Error('Could not find the container element');
+        }
 
-        // Find all elements with the class expanding-icon
         const expandableIconElements = document.querySelectorAll('.expanding-icon');
+        const extractedData = [];
 
-        // Loop through the icon elements and trigger the click event on the parent element
         for (const iconElement of expandableIconElements) {
             const parentElement = iconElement.parentElement;
             if (parentElement) {
@@ -40,12 +25,32 @@ async function scrollToBottom() {
 
                 // Click the element to expand it
                 parentElement.click();
-            }
 
-            // Wait for 400ms before clicking the next element
-            await sleep(400);
+                // Wait for the hidden div to appear
+                await sleep(800);
+
+                // Extract the data you want from the expanded div
+                const expandedDiv = document.querySelector('.virtual-scroll-item[style=""] > div');
+                if (expandedDiv) {
+                    // Replace this with the actual data extraction logic
+                    const domainName = expandedDiv.querySelector('.dt')?.innerText;
+                    const ipv4 = expandedDiv.querySelector('.dd')?.innerText;
+
+                    extractedData.push({ domainName, ipv4 });
+                }
+
+                // Click the element again to collapse it
+                parentElement.click();
+
+                // Wait for 400ms before clicking the next element
+                await sleep(400);
+            }
         }
+
+        console.log('Extracted data:', extractedData);
     } catch (error) {
         console.error('An error occurred during script execution:', error);
     }
-})();
+}
+
+main();
