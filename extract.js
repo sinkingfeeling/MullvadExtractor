bad130d102f18f92e4d30fd4553388a58bdae89d
skarz <40373461+sinkingfeeling@users.noreply.github.com>
2023-05-10 06:36:28 -0700

    Update extract.js
    add delay

--- extract.js
+++ extract.js
@@ -1,10 +1,19 @@
-// Find all elements with the class expanding-icon
-const expandableIconElements = document.querySelectorAll('.expanding-icon');
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// Loop through the icon elements and trigger the click event on the parent element
-expandableIconElements.forEach(iconElement => {
-    const parentElement = iconElement.parentElement;
-    if (parentElement) {
-        parentElement.click();
+(async function() {
+    // Find all elements with the class expanding-icon
+    const expandableIconElements = document.querySelectorAll('.expanding-icon');
+
+    // Loop through the icon elements and trigger the click event on the parent element
+    for (const iconElement of expandableIconElements) {
+        const parentElement = iconElement.parentElement;
+        if (parentElement) {
+            parentElement.click();
+        }
+
+        // Wait for 200ms before clicking the next element
+        await sleep(200);
     }
-});
+})();
