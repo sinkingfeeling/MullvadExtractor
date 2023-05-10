3f727b43f3a6bc724dde99938d215a98ebc82879
skarz <40373461+sinkingfeeling@users.noreply.github.com>
2023-05-10 06:38:24 -0700

    Update extract.js
    trying to adjust the scrolling distance based on the height of the divs since they're inconsistent

--- extract.js
+++ extract.js
@@ -4,30 +4,37 @@ function sleep(ms) {
 
 (async function() {
     try {
-        // Wait for 2 seconds before starting the script execution
-        await sleep(2000);
+        // Wait for 3 seconds before starting the script execution
+        await sleep(3000);
 
         // Find all elements with the class expanding-icon
         const expandableIconElements = document.querySelectorAll('.expanding-icon');
 
         // Loop through the icon elements and trigger the click event on the parent element
+        let accumulatedHeight = 0;
         for (const iconElement of expandableIconElements) {
             const parentElement = iconElement.parentElement;
             if (parentElement) {
-                // Scroll the parent element into view using window.scrollTo
+                // Calculate the scroll position based on the accumulated height of the expanded divs
                 const yOffset = window.pageYOffset;
-                const elementTop = parentElement.offsetTop;
+                const elementTop = parentElement.offsetTop - accumulatedHeight;
                 window.scrollTo({ top: yOffset + elementTop - window.innerHeight / 2, behavior: 'smooth' });
 
-                // Wait for 500ms before clicking the element to account for scrolling and rendering
-                await sleep(500);
+                // Wait for 800ms before clicking the element to account for scrolling and rendering
+                await sleep(800);
 
                 // Click the element to expand it
                 parentElement.click();
+
+                // Update the accumulated height with the height of the expanded div
+                const expandedDiv = parentElement.nextElementSibling;
+                if (expandedDiv) {
+                    accumulatedHeight += expandedDiv.getBoundingClientRect().height;
+                }
             }
 
-            // Wait for 200ms before clicking the next element
-            await sleep(200);
+            // Wait for 400ms before clicking the next element
+            await sleep(400);
         }
     } catch (error) {
         console.error('An error occurred during script execution:', error);
