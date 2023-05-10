a217673f334005b51e2c37380ec0456f30575f6c
skarz <40373461+sinkingfeeling@users.noreply.github.com>
2023-05-10 06:35:55 -0700

    Update extract.js
    simulate clicking

--- extract.js
+++ extract.js
@@ -1,14 +1,10 @@
 // Find all elements with the class expanding-icon
 const expandableIconElements = document.querySelectorAll('.expanding-icon');
 
-// Loop through the icon elements and set aria-expanded to true on the parent element
+// Loop through the icon elements and trigger the click event on the parent element
 expandableIconElements.forEach(iconElement => {
-    // Change the aria-expanded attribute of the parent element
     const parentElement = iconElement.parentElement;
     if (parentElement) {
-        parentElement.setAttribute('aria-expanded', 'true');
+        parentElement.click();
     }
-
-    // Add the rotate class to the expanding-icon element
-    iconElement.classList.add('rotate');
 });
