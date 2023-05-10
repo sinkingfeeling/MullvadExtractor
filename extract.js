15c2059e5178815f1f0dc57f596918024c011427
skarz <40373461+sinkingfeeling@users.noreply.github.com>
2023-05-10 06:35:25 -0700

    Update extract.js

--- extract.js
+++ extract.js
@@ -1,13 +1,14 @@
-// Find all elements with the aria-expanded attribute
-const expandableElements = document.querySelectorAll('[aria-expanded]');
+// Find all elements with the class expanding-icon
+const expandableIconElements = document.querySelectorAll('.expanding-icon');
 
-// Loop through the expandable elements and set aria-expanded to true
-expandableElements.forEach(element => {
-    element.setAttribute('aria-expanded', 'true');
-    
-    // Find the corresponding span with the class expanding-icon and add the rotate class
-    const spanElement = element.querySelector('.expanding-icon');
-    if (spanElement) {
-        spanElement.classList.add('rotate');
+// Loop through the icon elements and set aria-expanded to true on the parent element
+expandableIconElements.forEach(iconElement => {
+    // Change the aria-expanded attribute of the parent element
+    const parentElement = iconElement.parentElement;
+    if (parentElement) {
+        parentElement.setAttribute('aria-expanded', 'true');
     }
+
+    // Add the rotate class to the expanding-icon element
+    iconElement.classList.add('rotate');
 });
