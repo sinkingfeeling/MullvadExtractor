f14c15a2cf15f6e087996ac989b2fc3f44a3e295
skarz <40373461+sinkingfeeling@users.noreply.github.com>
2023-05-10 06:37:26 -0700

    Update extract.js
    something wrong with the scrolling

--- extract.js
+++ extract.js
@@ -3,24 +3,33 @@ function sleep(ms) {
 }
 
 (async function() {
-    // Find all elements with the class expanding-icon
-    const expandableIconElements = document.querySelectorAll('.expanding-icon');
+    try {
+        // Wait for 2 seconds before starting the script execution
+        await sleep(2000);
 
-    // Loop through the icon elements and trigger the click event on the parent element
-    for (const iconElement of expandableIconElements) {
-        const parentElement = iconElement.parentElement;
-        if (parentElement) {
-            // Scroll the parent element into view
-            parentElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        // Find all elements with the class expanding-icon
+        const expandableIconElements = document.querySelectorAll('.expanding-icon');
 
-            // Wait for 500ms before clicking the element to account for scrolling and rendering
-            await sleep(500);
+        // Loop through the icon elements and trigger the click event on the parent element
+        for (const iconElement of expandableIconElements) {
+            const parentElement = iconElement.parentElement;
+            if (parentElement) {
+                // Scroll the parent element into view using window.scrollTo
+                const yOffset = window.pageYOffset;
+                const elementTop = parentElement.offsetTop;
+                window.scrollTo({ top: yOffset + elementTop - window.innerHeight / 2, behavior: 'smooth' });
 
-            // Click the element to expand it
-            parentElement.click();
+                // Wait for 500ms before clicking the element to account for scrolling and rendering
+                await sleep(500);
+
+                // Click the element to expand it
+                parentElement.click();
+            }
+
+            // Wait for 200ms before clicking the next element
+            await sleep(200);
         }
-
-        // Wait for 200ms before clicking the next element
-        await sleep(200);
+    } catch (error) {
+        console.error('An error occurred during script execution:', error);
     }
 })();
